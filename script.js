e9cd07c81291a8e48996a7a863dbc821c14e6687
Damian <79758592+damian-fokt@users.noreply.github.com>
2024-06-14 11:33:14 +0200

    Logo background. Adding computer list. Fixes.

--- script.js
+++ script.js
@@ -5,6 +5,41 @@ document.addEventListener("DOMContentLoaded", function () {
   navToggle.addEventListener("click", function () {
     navLinks.classList.toggle("show");
   });
+
+  // Drag to scroll functionality
+  const computerList = document.querySelector(".computer-list");
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  computerList.addEventListener("mousedown", (e) => {
+    isDown = true;
+    computerList.classList.add("active");
+    computerList.classList.add("noselect");
+    startX = e.pageX - computerList.offsetLeft;
+    scrollLeft = computerList.scrollLeft;
+  });
+
+  computerList.addEventListener("mouseleave", () => {
+    isDown = false;
+    computerList.classList.remove("active");
+    computerList.classList.remove("noselect");
+  });
+
+  computerList.addEventListener("mouseup", () => {
+    isDown = false;
+    computerList.classList.remove("active");
+    computerList.classList.remove("noselect");
+  });
+
+  computerList.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - computerList.offsetLeft;
+    const walk = (x - startX) * 3; // scroll-fast
+    computerList.scrollLeft = scrollLeft - walk;
+  });
+
   var logo = document.getElementById("logo");
   logo.style.backgroundColor = "transparent";
 });
